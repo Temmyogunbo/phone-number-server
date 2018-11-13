c898bc7e483d0f6f41e4f641c12bf96f572f9ecd
temmyogunbo <Ogunbotemilola@yahoo.com>
2018-11-13 09:41:43 +0100

    setup lab for test

---
+++ .labrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  coverage: true,
+  leaks: false,
+  transform: "node_modules/lab-babel",
+  verbose: true,
+  reporter: ["console", "lcov", "json", "html"],
+  output: [
+    "stdout",
+    "coverage/lcov.info",
+    "coverage/data.json",
+    "coverage/coverage.html"
+  ],
+  timeout: 200000,
+  "context-timeout": 200000
+};
